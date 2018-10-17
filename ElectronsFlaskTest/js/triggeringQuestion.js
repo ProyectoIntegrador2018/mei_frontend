c213e8e068e117eec638bd9d0bef090d8674a07f
CharlieBradbury <babe-hime@hotmail.com>
2018-10-17 01:03:34 -0500

    Show session ideas

--- ElectronsFlaskTest/js/triggeringQuestion.js
+++ ElectronsFlaskTest/js/triggeringQuestion.js
@@ -1,8 +1,12 @@
+//GLOBAL CONSTANTS
+IDEA_TYPE = ""
+
 $(document).ready(function(){
   //TODO: NO SIEMPRE SE LOADEA EL CONTENIDO
   getTriggering()
+  getIdeas()
+  addParticipants()
 })
-
 
 function getTriggering(){
   $.ajax({
@@ -15,6 +19,63 @@ function getTriggering(){
       if (response['Success']){
         var sessionCard = getSessionCard(localStorage.getItem("SessionId"), response['triggeringQuestion'])
         $("#projectSessions").append(sessionCard)
+      }
+    },
+    error : function (error) {
+      console.log("Error: " + error);
+    }
+  });
+}
+
+function getIdeas(){
+  $.ajax({
+    url : "http://127.0.0.1:5000/get_session_ideas",
+    type : "POST",
+    data : {
+      sessionID : localStorage.getItem("SessionId")
+    },
+    success : function (response) { 
+      if (response['Success']) {
+        var i = 0
+        var ideas = response['Ideas']
+        var keys = Object.keys(ideas)
+        if (keys.length > 0){
+          for(i = 0; i < keys.length; i++){
+            for(j = 0; j < ideas[keys[i]].length; j++){
+              var childID = ideas[keys[i]][j][0]
+              var childIdeaText = ideas[keys[i]][j][2]
+              var childClarification = (ideas[keys[i]][j][3] == null) ? 'No clarification' : ideas[keys[i]][j][3]
+              IDEA_TYPE = ideas[keys[i]][j][5]
+              var childParticipantEmail = (ideas[keys[i]][j][4] == null) ? 'Anonymous' : ideas[keys[i]][j][4]
+              var childIdea = getIdeaCard(childID,childIdeaText, childClarification, childParticipantEmail)
+              $("#ideaSessions").append(childIdea)
+            }
+          }
+        }
+      }
+    },
+    error : function (error) {
+      console.log("Error: " + error);
+    }
+  });
+}
+
+function addParticipants(){
+  $.ajax({
+    url : "http://127.0.0.1:5000/get_session_participants",
+    type : "POST",
+    data : {
+      sessionID : localStorage.getItem("SessionId")
+    },
+    success : function (response) { 
+      if (response['Success']){
+        console.log( response.Members.length)
+        var option = document.createElement("option");
+        for(member in response.Members) {
+          option.text = response.Members[member].email;
+          option.value = response.Members[member].email;
+          $("#participantSelection").append(option)
+        }
       }
     },
     error : function (error) {
@@ -37,15 +98,68 @@ function getSessionCard(id,triggeringQuestion){
   return sessionCard
 }
 
+function getIdeaCard(id,statement, clarification, participant){
+  var ideaCard = `
+    <div class="col-sm-4">
+    <div id="ideaCard" class="card" style="width: 18rem;"">
+      <div class="card-body shadow-sm">
+        <h3 class="card-text">${statement}</h3>
+        <h4 class="card-text">${clarification}</h4>
+        <span class="badge badge-secondary" float-left>${IDEA_TYPE}</span>
+        <h6 class="card-text" float-right">${participant}</h6>
+      </div>
+    </div>
+    </div>`
+  return ideaCard
+}
+
+$("#addIdeabtn").click(function() {
+  var statement =  $("#statement").val();
+  var clarification = $("#clarification").val();
+  var participant = $("#participantSelection").val();
+
+  if(IDEA_TYPE != "") {
+    $.ajax({
+      url : "http://127.0.0.1:5000/create_element",
+      type : "POST",
+      data : {
+        idea : statement,
+        clarification : clarification,
+        ideaType : IDEA_TYPE,
+        participant : participant,
+        sessionID : localStorage.getItem("SessionId")
+      },
+      success : function (response) {
+        if (response['Success']){
+          var ideaCard = getIdeaCard(localStorage.getItem("SessionId"),statement, clarification, participant)
+          $("#ideaSessions").append(ideaCard)
+        }
+      },
+      error : function (error) {
+        console.log("Error: " + error);
+      }
+    })
+  } else {
+    alert("There is no element type for the session")
+  }
+});
+
+$("#addElementTypeBtn").click(function() {
+  if (IDEA_TYPE == "") {
+      IDEA_TYPE = $("#elementSelection").value
+      $("#trigQuestion").append( '<span class="badge badge-primary">' + IDEA_TYPE + '</span>')
+      $( "#otherOption").hide()
+  }});
+
 $( "#elementSelection" ).change(function() {
   if(this.value == "Other") {
     $( "#otherOption").show()
-}});
-
-$("#elementType").click(function() {
-  $("#trigQuestion").append( `<span class="badge badge-primary">${$("#elementSelection").value}</span>`)
-  $( "#otherOption").hidden()
+    IDEA_TYPE = $( "#otherOption").value
+  } else {
+    IDEA_TYPE = this.value
+  }
 });
+
 
 
 
