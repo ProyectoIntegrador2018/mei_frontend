25fa941b36321a1121e911417de494db6df9a712
LuisHernandez96 <luis_fernando_hernandez@outlook.com>
2018-10-17 02:53:11 -0500

    Added ideas to vote

--- ElectronsFlaskTest/js/sessionVoting.js
+++ ElectronsFlaskTest/js/sessionVoting.js
@@ -1,6 +1,31 @@
 $(document).ready(function(){
+  getIdeasVoting()
   getSessionParticipantsVoting(localStorage.getItem("SessionId"))
 })
+
+function getIdeasVoting(){
+  $.ajax({
+    url : "http://127.0.0.1:5000/get_session_ideas",
+    type : "POST",
+    data : {
+      sessionID : localStorage.getItem("SessionId")
+    },
+    success : function (response) {
+      if (response['Success']) {
+        var i = 1
+        response['Ideas'][1].forEach(function (idea){
+          var ideaText = idea[2]
+          addIdeaCardVoting(i,ideaText,"")
+          i++
+        })
+      }
+    },
+    error : function (error) {
+      console.log("Error: " + error);
+    }
+  });
+}
+
 function getSessionParticipantsVoting(sessionID){
 	$.ajax({
 		url : "http://127.0.0.1:5000/get_session_participants",
@@ -15,18 +40,34 @@ function getSessionParticipantsVoting(sessionID){
           options += `<option value="${member['email']}">${member['name']}</option>`
         })
         $("#inputParticipantNameVoting").append(options)
-        // var participants = response['Members']
-				// keys = Object.keys(participants)
-				// if (keys.length > 0){
-				// 	keys.forEach(function(key){
-				// 		var participant = participants[key]
-        //
-				// 		$("#sessionParticipants").append( participant['name'], participant['email'] )
-				// 	})
       }
 		},
 		error : function(error){
 			console.log("Error: " + error);
 		}
 	});
+}
+
+function addIdeaCardVoting(id,statement,priority){
+  var ideaCard = `
+    <div class="col-sm-4">
+    <div id="ideaCardVoting" class="card" style="width: 36rem;"">
+      <div class="card-body shadow-sm">
+        <div class="row">
+          <div class="col-1">
+            <h4 class="card-text">${id}</h4>
+          </div>
+          <div class="col-8">
+            <h4 class="card-text">${statement}</h4>
+          </div>
+          <div class="col-3">
+            <select class="form-control mt-2 shadow-sm" id="inputIdeaVoting${id}">
+              <option value="" selected disabled hidden>Priority</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>`
+  $("#ideasSectionVoting").append(ideaCard)
 }
