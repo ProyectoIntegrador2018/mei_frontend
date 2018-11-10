80dba51054b6da0aabf2114ef2d8522f96776124
gerardogalvez <galvezgerardo01@gmail.com>
2018-11-10 14:42:23 -0600

    Estructura de prioridad (mal) y campo

--- ElectronsFlaskTest/js/campo.js
+++ ElectronsFlaskTest/js/campo.js
@@ -1,3 +1,5 @@
+const interact = require('interactjs');
+
 let ideas = []
 let ideaIDCategory = {}
 let categoryIdeas = {}
@@ -5,10 +7,6 @@ let question = {
   'firstElement': -1,
   'secondElement': -1
 }
-
-let questions = []
-
-let categoriesCreated = 0
 
 $(document).ready(function(){
 	sessionHasCategories()
@@ -36,7 +34,6 @@ function sessionHasCategories(){
 	    	}
 	    	else {
 	    		getIdeas();
-	    		getRelationshipQuestion();
 	    	}
 	    }
 	  },
@@ -65,6 +62,52 @@ function deleteSessionCategories() {
 	});
 }
 
+function removeIdeaFromCategory(ideaID) {
+	let ideasInCategory = $("#" + ideaID).parent().children(".idea")
+	if (ideasInCategory.length == 2) {
+		$($("#" + ideaID).parent().parent().parent()).remove()
+		for (var i = 0; i < ideasInCategory.length; i++) {
+			$(ideasInCategory[i]).addClass("draggable")
+			$(ideasInCategory[i]).addClass("dropzone")
+			$(ideasInCategory[i]).appendTo("#ideas")
+			$(ideasInCategory[i]).find("button.close").css("display", "none")
+		}
+	}
+	else{
+		$("#" + ideaID).addClass("draggable")
+		$("#" + ideaID).addClass("dropzone")
+		$("#" + ideaID).appendTo("#ideas")
+		$("#" + ideaID).find("button.close").css("display", "none")
+	}
+}
+
+function getIdeaCard(idea){
+	return `<div id="idea-${idea['ideaID']}" class="idea draggable dropzone" style="position: relative">
+				<div class="card mt-2">
+				  <div class="card-header">
+			  		<button type="button" class="close float-left mr-2 mt-1" aria-label="Close" onclick="removeIdeaFromCategory('idea-${idea['ideaID']}')" style="Display: none">
+			  		  &times;
+			  	    </button>
+				  	${idea['type']} ${idea['ideaSessionNumber']}
+				  </div>
+				  <div class="card-body">
+				    <h6 class="card-title">${idea['idea']}</h6>
+				  </div>
+				</div>
+			</div>`
+}
+
+function getCategoryCard(children){
+	return `<div class="category dropzone">
+				<div class="card mt-2">
+				  <div class="card-header"><b>New category</b></div>
+				  <div class="card-body">
+				  	${children}
+				  </div>
+				</div>
+			</div>`
+}
+
 function getIdeas(){
   $.ajax({
     url : "http://127.0.0.1:5000/get_all_session_ideas",
@@ -77,23 +120,25 @@ function getIdeas(){
         var i = 0
         var ideasReceived = response['Ideas']
         if (ideasReceived.length == 0) {
-          $("#questions").css('visibility', 'hidden')
+          $("#categories").css('visibility', 'hidden')
           $("#warning").css('visibility', 'visible')
           $("#warning>div>h1").text('No elements found in this session.')
         }
         else if (ideasReceived.length == 1) {
-          $("#questions").css('visibility', 'hidden')
+          $("#categories").css('visibility', 'hidden')
           $("#warning").css('visibility', 'visible')
           $("#warning>div>h1").text('At least two elements are needed in this session.')
         }
         else {
           $("#questions").css('visibility', 'visible')
           $("#warning").css('visibility', 'hidden')
+          ideasCards = ""
           for(i = 0; i < ideasReceived.length; i++){
             ideas.push(ideasReceived[i])
+            ideasCards = ideasCards + getIdeaCard(ideasReceived[i])
           }
 
-          setNextQuestion(0, 1)
+          $("#ideas").append(ideasCards)
         }
       }
     },
@@ -103,102 +148,124 @@ function getIdeas(){
   });
 }
 
-function setNextQuestion(i, j) {
-	question['firstElement'] = i
-	question['secondElement'] = j
-	$("#firstElementHeader").text(`${ideas[i]['type']} ${ideas[i]['ideaSessionNumber']}`)
-    $("#firstElementStatement").text(ideas[i]['idea'])
-    $("#firstElement>div").attr('id', ideas[i]['ideaID'])
-    $("#secondElementHeader").text(`${ideas[j]['type']} ${ideas[j]['ideaSessionNumber']}`)
-    $("#secondElementStatement").text(ideas[j]['idea'])
-    $("#secondElement>div").attr('id', ideas[j]['ideaID'])
-}
+interact('.draggable').draggable({
+  // enable inertial throwing
+    inertia: false,
+    // keep the element within the area of it's parent
+    restrict: {
+      restriction: "parent",
+      endOnly: true,
+      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    },
 
-$("#yes").click(function() {
-  yesVotes = parseInt($("#yes-votes").val())
-  noVotes = parseInt($("#no-votes").val())
-  firstElementID = $("#firstElement>div").attr('id')
-  secondElementID = $("#secondElement>div").attr('id')
+    onstart: function (event) {
+        // event.target.style.zIndex = ;
+        $(event.target).css("z-index", 1);
+    },
 
-  if (!(firstElementID in ideaIDCategory)){
-  	ideaIDCategory[firstElementID] = categoriesCreated
-  	ideaIDCategory[secondElementID] = categoriesCreated
-  	categoriesCreated += 1
-  }
-  else{
-  	ideaIDCategory[secondElementID] = ideaIDCategory[firstElementID]
-  }
+    onend: function (event) {
+    	var draggableElement = event.target
+    	// translate the element
+    	draggableElement.style.webkitTransform =
+    	draggableElement.style.transform =
+    	  'translate(' + 0 + 'px, ' + 0 + 'px)';
 
-  console.log(ideaIDCategory)
+    	  // update the position attributes
+    	draggableElement.setAttribute('data-x', 0);
+    	draggableElement.setAttribute('data-y', 0);
+    },
 
-  questions.push({'firstElement': ideas[question['firstElement']]['ideaID'], 'secondElement': ideas[question['secondElement']]['ideaID'], 'yesVotes': yesVotes, 'noVotes': noVotes, 'answer': true})
+    // enable autoScroll
+    autoScroll: true,
 
-  getNextQuestion()
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
 })
 
-$("#no").click(function() {
-  yesVotes = parseInt($("#yes-votes").val())
-  noVotes = parseInt($("#no-votes").val())
-  firstElementID = $("#firstElement>div").attr('id')
-  secondElementID = $("#secondElement>div").attr('id')
+// enable draggables to be dropped into this
+interact('.dropzone').dropzone({
+  // only accept elements matching this CSS selector
+  accept: '.idea',
+  // Require a 30% element overlap for a drop to be possible
+  overlap: 0.3,
 
-  if (!(firstElementID in ideaIDCategory)){
-  	ideaIDCategory[firstElementID] = categoriesCreated
-  	categoriesCreated += 1
-  }
-  
-  if (!(secondElementID in ideaIDCategory)){
-  	ideaIDCategory[secondElementID] = categoriesCreated
-  	categoriesCreated += 1
-  }
+  ondrop: function (event) {
+  	console.log("Drop!!")
+    var draggableElement = event.relatedTarget,
+        dropzoneElement = event.target;
 
-  console.log(ideaIDCategory)
+    console.log(dropzoneElement)
 
-  questions.push({'firstElement': ideas[question['firstElement']]['ideaID'], 'secondElement': ideas[question['secondElement']]['ideaID'], 'yesVotes': yesVotes, 'noVotes': noVotes, 'answer': false})
+    // translate the element
+    draggableElement.style.webkitTransform =
+    draggableElement.style.transform =
+      'translate(' + 0 + 'px, ' + 0 + 'px)';
 
-  getNextQuestion()
+      // update the position attributes
+    draggableElement.setAttribute('data-x', 0);
+    draggableElement.setAttribute('data-y', 0);
+
+    if ($(dropzoneElement).hasClass("idea")) {
+    	draggableElement.classList.remove('draggable')
+    	draggableElement.classList.remove('dropzone')
+    	dropzoneElement.classList.remove('draggable')
+    	dropzoneElement.classList.remove('dropzone')
+    	$(draggableElement).find("button.close").css("display", "inline")
+    	$(dropzoneElement).find("button.close").css("display", "inline")
+    	let category = getCategoryCard($(dropzoneElement)[0].outerHTML + $(draggableElement)[0].outerHTML)
+    	$(draggableElement).remove()
+    	$(dropzoneElement).remove()
+    	$("#ideas").prepend(category)
+    }
+    else if ($(dropzoneElement).hasClass("category")) {
+    	draggableElement.classList.remove('draggable')
+    	draggableElement.classList.remove('dropzone')
+    	$(draggableElement).find("button.close").css("display", "inline")
+    	$($($(dropzoneElement).children()[0]).children()[1]).append($(draggableElement)[0].outerHTML)
+    	$(draggableElement).remove()
+    }
+  },
+});
+
+$("#saveCategories").click(function() {
+	saveCategories()
 })
-
-function getNextQuestion() {
-	currentQuestion = question
-	nextQuestion = {'firstElement': -1, 'secondElement': -1}
-
-	if (currentQuestion['secondElement'] >= (ideas.length - 1)) {
-		nextQuestion['firstElement'] = currentQuestion['firstElement'] + 1
-		nextQuestion['secondElement'] = nextQuestion['firstElement'] + 1
-	}
-	else{
-		nextQuestion['firstElement'] = currentQuestion['firstElement']
-		nextQuestion['secondElement'] = currentQuestion['secondElement'] + 1
-	}
-
-	if (nextQuestion['firstElement'] == (ideas.length - 1)){
-		console.log("Saving categories!")
-		saveCategories()
-	}
-	else {
-		question = nextQuestion
-		setNextQuestion(question['firstElement'], question['secondElement'])
-	}
-}
 
 function saveCategories(){
-	Object.keys(ideaIDCategory).forEach(function(ideaID) {
-	    category = ideaIDCategory[ideaID]
-	    if (!(category in categoryIdeas)){
-	    	categoryIdeas[category] = []
-	    }
 
-	    categoryIdeas[category].push(ideaID)
-	});
+	let categories = $("#ideas").children(".category")
+	let outerLevelIdeas = $("#ideas").children(".idea")
+
+	console.log(categories)
+	console.log(outerLevelIdeas)
+
+	let categoriesToCreate = {}
+
+	let categoryNumber = 1
+
+	for (var i = 0; i < categories.length; i++) {
+		let categoryIdeas = $($($(categories[i]).children()[0]).children()[1]).children(".idea")
+		console.log(categoryIdeas)
+		let ideas = []
+		for (var j = 0; j < categoryIdeas.length; j++) {
+			ideas.push($(categoryIdeas[j]).attr("id").substr($(categoryIdeas[j]).attr("id").indexOf("-") + 1))
+		}
+
+		categoriesToCreate[categoryNumber] = ideas
+		categoryNumber += 1
+	}
+
+	for (var i = 0; i < outerLevelIdeas.length; i++) {
+		categoriesToCreate[categoryNumber] = [$(outerLevelIdeas[i]).attr("id").substr($(outerLevelIdeas[i]).attr("id").indexOf("-") + 1)]
+		categoryNumber += 1
+	}
 
 	$.ajax({
 		url : "http://127.0.0.1:5000/save_categories",
 		type : "POST",
 		data : JSON.stringify({
 			sessionID: localStorage.getItem("SessionId"),
-			categories: categoryIdeas,
-			questions: questions
+			categories: categoriesToCreate,
 		})
 		,
 		contentType: "application/json",
@@ -213,22 +280,24 @@ function saveCategories(){
 		  console.log("Error: " + error);
 		}
 	})
+
+	console.log(categoriesToCreate)
 }
 
-function getRelationshipQuestion(){
-	$.ajax({
-		url : "http://127.0.0.1:5000/get_structure_question",
-		type : "POST",
-		data : {
-		  structureType : "CAMPO"
-		},
-		success : function (response) { 
-		  if (response['Success']) {
-		  	$("#relationQuestion").text(response['question'])
-		  }
-		},
-		error : function (error) {
-		  console.log("Error: " + error);
-		}
-	})
+function dragMoveListener (event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the position attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
 }
+
+window.dragMoveListener = dragMoveListener;
