91b5f1f30b886a991df081f403bfb7896fc9abdc
CharlieBradbury <babe-hime@hotmail.com>
2018-12-11 18:23:15 -0600

    Fix voting tab

--- ElectronsFlaskTest/js/main.js
+++ ElectronsFlaskTest/js/main.js
@@ -1,4 +1,4 @@
-let server_url = 'http://gerardogalvez.pythonanywhere.com'
-// let server_url = 'http://127.0.0.1:5000'
+//let server_url = 'http://gerardogalvez.pythonanywhere.com'
+let server_url = 'http://127.0.0.1:5000'
 
 module.exports.server_url = server_url
